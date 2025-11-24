262dd2a68d4a7233787225d2df4bad36c86be6c1
Hector Gabriel Becerra Rivera <rivehect5t@gmail.com>
2025-11-24 15:15:36 -0600

    commit navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import DarmaxWelcome from "./pages/DarmaxWelcome";
 
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<DarmaxWelcome />} />
+
+
+
+
+
     </Routes>
   );
 }
