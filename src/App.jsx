ac168028058013909428183f7555b2ba12aa621e
GarkarAXO <e.axel12@gmail.com>
2025-11-24 15:00:28 -0600

    Update(estructure): new deploy for the proyect

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-
-import AguaPuraPaso2AsignarGarrafones from "./SistemaDeVentas/compraDeGarrafones/AguaPuraPaso2AsignarGarrafones.jsx";
-import AguaPuraSeleccionTamano from "./SistemaDeVentas/compraDeGarrafones/AguaPuraSeleccionTamano.jsx";
-import AguaPuraPaso2AsignarGarrafonesInicial from "./SistemaDeVentas/AguaPuraPaso2AsignarGarrafonesInicial.jsx";
-import AguaPuraPaso3MetodoEntrega from "./SistemaDeVentas/AguaPuraPaso3MetodoEntrega.jsx";
-import AguaPuraResumenPago from "./SistemaDeVentas/AguaPuraResumenPago.jsx";
-import SeleccionDeGarrafon from "./SistemaDeVentas/SeleccionDeGarrafon.jsx";
-import Seleccion from "./SistemaDeVentas/Seleccion.jsx";
+import DarmaxWelcome from "./pages/DarmaxWelcome";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Bienvenido a AguaPura</h1>} />
-      <Route path="/comprar" element={<Seleccion />} />
-      <Route path="/comprar/seleccion-garrafon" element={<SeleccionDeGarrafon />} />
-      <Route path="/comprar/seleccionar-tamano" element={<AguaPuraSeleccionTamano />} />
-      <Route path="/comprar/asignar-garrafones-inicial" element={<AguaPuraPaso2AsignarGarrafonesInicial />} />
-      <Route path="/comprar/asignar-garrafones" element={<AguaPuraPaso2AsignarGarrafones />} />
-      <Route path="/comprar/metodo-entrega" element={<AguaPuraPaso3MetodoEntrega />} />
-      <Route path="/comprar/resumen" element={<AguaPuraResumenPago />} />
+      <Route path="/" element={<DarmaxWelcome />} />
     </Routes>
   );
 }
