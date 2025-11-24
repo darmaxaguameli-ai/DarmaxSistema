260c0079fd44ddc6ecc1c09e8c4c85bff82996b0
Hector Gabriel Becerra Rivera <rivehect5t@gmail.com>
2025-11-24 16:45:42 -0600

    GESTION

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import DarmaxWelcome from "./pages/DarmaxWelcome";
-
-
+import GestionDashboard from "./Gestion/GestionDashboard";
+import Resumen from "./Gestion/Resumen";
+import Inventario from "./Gestion/Inventario";
+import Ingresos from "./Gestion/Ingresos";
+import Gastos from "./Gestion/Gastos";
 
 function App() {
   return (
-
     <Routes>
       <Route path="/" element={<DarmaxWelcome />} />
-
-
-
-
-
+      <Route path="/gestion" element={<GestionDashboard />}>
+        <Route index element={<Resumen />} />
+        <Route path="inventario" element={<Inventario />} />
+        <Route path="ingresos" element={<Ingresos />} />
+        <Route path="gastos" element={<Gastos />} />
+      </Route>
     </Routes>
   );
 }
 
 export default App;
+
